2f7562168b8b4d9856be6be1bdd83d8d1eebeaa8
Teslafly <marshscholz@gmail.com>
2014-04-17 23:39:02 -0500

    Created new program for multiple sensor onewire bus discovery
    new program added for dynamic scanning of onewire bus and automatic
    detection of up to 20 sensors (easily changeable) arduino program is
    well on it's way to working but still has some bugs and incomplete bits.

--- emonTH_DHT22_multiple_DS18B20/emonTH_DHT22_multiple_DS18B20.ino
+++ emonTH_DHT22_multiple_DS18B20/emonTH_DHT22_multiple_DS18B20.ino
@@ -2,6 +2,8 @@
  emonTH low power temperature & humidity node
  ============================================
  
+ !!! this is currently a hogepoge of code! do not use!!!
+ 
  Ambient humidity & temperature (DHT22 on-board)
  Multiple remote temperature (DS18B20)
  
@@ -10,10 +12,10 @@
  1. Battery voltage
  2. Humidity (with DHT22 on-board sensor, otherwise zero)
  3. Ambient temperature (with DHT22 on-board sensor, otherwise zero)
- 4. External temperature 1 (first external DS18B20)
- 5. External temperature 2 (second external DS18B20)
- 
- Easily extended to support further external DS18B20 sensors, see comments below.
+ 4. External temperature 1 (first DS18B20)
+ 5. External temperature 2 (second DS18B20)....
+ 6. and so on. Should automatically detect any DS18B20 connected to the one wire bus. 
+ note - if you connect additional DS18B20 sensors, the rf packet sensor order may change. check your inputs in emoncms after adding additional sensors and reassign accordingly.
  
  -----------------------------------------------------------------------------------------------------------  
  Technical hardware documentation wiki: http://wiki.openenergymonitor.org/index.php?title=EmonTH
@@ -21,9 +23,9 @@
  Part of the openenergymonitor.org project
  Licence: GNU GPL V3
  
- Authors: Dave McCraw,
+ Authors: Dave McCraw, Marshall Scholz
  
- Based on the emonTH_DHT22_DS18B20 sketch by Glyn Hudson.
+ Based on the emonTH_DHT22_DS18B20 sketch by Glyn Hudson and the dallas temp library tester-simple sketch.
  
  THIS SKETCH REQUIRES:
  
@@ -33,15 +35,18 @@
    - OneWire library      http://www.pjrc.com/teensy/td_libs_OneWire.html   - DS18B20 sensors
    - DallasTemperature    http://download.milesburton.com/Arduino/MaximTemperature/DallasTemperature_LATEST.zip - DS18B20 sensors
  */
+ 
 #include <avr/power.h>
 #include <avr/sleep.h>
 #include <RFu_JeeLib.h>                                                 
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include "DHT.h"
+
+// Attach JeeLib sleep function to Atmega328 watchdog - enables MCU to be put into sleep mode between readings to reduce power consumption 
 ISR(WDT_vect) { 
   Sleepy::watchdogEvent(); 
-} // Attached JeeLib sleep function to Atmega328 watchdog - enables MCU to be put into sleep mode between readings to reduce power consumption 
+} 
 
  
 /*
@@ -68,27 +73,40 @@ ISR(WDT_vect) {
 const int NETWORK_GROUP = 210;
 const int NODE_ID       = 19;
 
-/*
- Monitoring configuration
- ========================
+
+// Monitoring configuration
+// ========================
  
-  - how long to wait between readings, in minutes
+ const int SECS_BETWEEN_READINGS = 60;  // How long to wait between readings, in seconds. - default = 60
+ 
+ const int ASYNC_DELAY           = 375; // delay for onewire sensors to send data
+ 
+ const int TEMPERATURE_PRECISION = 11;  // onewire temperature sensor precisionn. details found below. - default = 11
+ /*
+  NOTE: There is a trade off between power consumption and sensor resolution.
+        A higher resolution will keep the processor awake longer - approximate values found below.
+                                     
   - DS18B20 temperature precision:
       9bit: 0.5C,  10bit: 0.25C,  11bit: 0.1125C, 12bit: 0.0625C
   - Required delay when reading DS18B20
       9bit: 95ms,  10bit: 187ms,  11bit: 375ms,   12bit: 750ms
+     
+  More info can be found here: http://harizanov.com/2013/07/optimizing-ds18b20-code-for-low-power-applications/   
  */
-const int SECS_BETWEEN_READINGS = 60; // seconds between readings
-const int TEMPERATURE_PRECISION = 11; 
-const int ASYNC_DELAY           = 375;
+
 
 // emonTH pin allocations 
-const int BATT_ADC     = 1;
-const int DS18B20_PWR  = 5;
+const int BATT_ADC     = 1; // adc 1
+const int DS18B20_PWR  = 5; 
 const int DHT_PWR      = 6;
 const int LED          = 9;
 const int DHT_PIN      = 18; 
 const int ONE_WIRE_BUS = 19;  
+
+// end of configuration
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// start of part that does stuff
 
 // On board DHT22
 DHT dht(DHT_PIN, DHT22);
@@ -98,39 +116,28 @@ boolean DHT_PRESENT;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
+int numberOfDevices; // Number of temperature devices found
+DeviceAddress tempDeviceAddress; // We'll use this variable to store a found onewire device address
+
+//int onewireTemp[20];  // supports howevermany onewire temperature sensors that this number allows for. increase if you have more sensors. 
+                         // watch out for ram overflows though.....  
+
 // RFM12B RF payload datastructure
 typedef struct {       
   int battery;    
   int humidity;                                                  
   int internalTemp;       	                                      
-  int externalTemp1;       	                                      
-  int externalTemp2;
+  int onewireTemp[20];	                                      
   // If you have more sensors, add further variables here.
 } 
 Payload;
 
-Payload rfPayload;
+Payload rfPayload; // make a new variable "rfPayload" with type "payload"
 
-boolean debug;
 
-/*
-  External sensor addresses
-  =========================
-  
- Hardcoding these guarantees emonCMS inputs won't flip around if you replace or add sensors.
- Use one of the address finding sketches to determine your sensors' unique addresses.
-
- See 'emonTH temperature search' utility sketch in 'Simple emonTH Sensor Test' folder
+boolean debug; // variable to store is debug is avalable or not. 
  
- Extend this if you have more sensors.
- */
-DeviceAddress EXT_SENSOR1 = { 
-  0x28, 0x46, 0x59, 0x44, 0x05, 0x00, 0x00, 0x64 };
-DeviceAddress EXT_SENSOR2 = { 
-  0x28, 0x9B, 0x24, 0x44, 0x05, 0x00, 0x00, 0x6B };
-
-boolean EXT_SENSOR1_PRESENT;  
-boolean EXT_SENSOR2_PRESENT;  
+ 
 
 /**
  * setup() - called once on boot to initialise the emonTH
@@ -150,9 +157,10 @@ void setup() {
   rf12_initialize(NODE_ID, FREQUENCY, NETWORK_GROUP);                       
   rf12_sleep(RF12_SLEEP);
 
+  // turn off non crital chip functions to reduce battery drain
   reduce_power();
   
-  // Initialise sensors
+  // Find & Initialise sensors
   initialise_DHT22();
   initialise_DS18B20();
 
@@ -169,8 +177,10 @@ void setup() {
  */
 void loop()
 { 
+
+
   // External temperature readings
-  if (EXT_SENSOR1_PRESENT || EXT_SENSOR2_PRESENT) 
+  if (numberOfDevices >= 0) 
     take_ds18b20_reading();
 
   // Internal temperature / humidity readings
@@ -183,6 +193,7 @@ void loop()
   // Debugging
   print_payload();                                             
 
+  // power up radio and send data packet
   power_spi_enable();  
   rf12_sleep(RF12_WAKEUP);
   rf12_sendNow(0, &rfPayload, sizeof rfPayload);
@@ -199,26 +210,7 @@ void loop()
 }
 
 
-
-/**
- * Turn off what we don't need.
- * see http://www.nongnu.org/avr-libc/user-manual/group__avr__power.html
- */
-void reduce_power()
-{
-  ACSR |= (1 << ACD);              // Disable Analog comparator    
-  power_twi_disable();             // Disable the Two Wire Interface module.
-
-  power_timer1_disable();          // Timer 1
-  power_spi_disable();             // Serial peripheral interface
-
-    if (!debug){
-    power_usart0_disable();        // Disable serial UART if not connected
-  }  
-
-  power_timer0_enable();           // Necessary for the DS18B20 library.
-}
-
+//////////////////////////////////////////////////
 /**
  * Set the pin modes required for this sketch
  */
@@ -230,6 +222,17 @@ void set_pin_modes()
   pinMode(BATT_ADC,    INPUT);
 }
 
+/////////////////////////////////////////////////
+/**
+ * Flash the LED for the stated period
+ */
+void flash_led (int duration){
+  digitalWrite(LED,HIGH);
+  dodelay(duration);
+  digitalWrite(LED,LOW); 
+}
+
+//////////////////////////////////////////////////
 /**
  * Find the DHT22 sensor, if fitted
  */
@@ -265,13 +268,13 @@ void initialise_DHT22()
   digitalWrite(DHT_PWR,LOW);                                          
 }
 
-
+//////////////////////////////////////////////////
 /**
  * Find the expected DS18B20 sensors
  *
- * You will need your sensors' unique address (obtain using one of the sketches designed for this purpose).
+ * automatically scans the entire onewire bus for sensors and stores their adressess in the device adresses array.
  * 
- * Straightforward to add support for additional sensors by extending this function.
+ * should support a large number of sensors. tested up to four sensors currently.
  */
 void initialise_DS18B20()
 {
@@ -279,49 +282,96 @@ void initialise_DS18B20()
   digitalWrite(DS18B20_PWR, HIGH); 
   dodelay(50); 
 
+  // start up oonewire library
   sensors.begin();
 
   // Disable automatic temperature conversion to reduce time spent awake, instead we sleep for ASYNC_DELAY
   // see http://harizanov.com/2013/07/optimizing-ds18b20-code-for-low-power-applications/ 
   sensors.setWaitForConversion(false);                             
 
-  // Note - the index (param 2) is dependant on the natural ordering of your sensor addresses.
-  // We'll try both ways, just in case.
-  EXT_SENSOR1_PRESENT = sensors.getAddress(EXT_SENSOR1, 0);
-  EXT_SENSOR2_PRESENT = sensors.getAddress(EXT_SENSOR2, 1);
+  // Grab a count of devices on the wire
+  numberOfDevices = sensors.getDeviceCount();
 
-  EXT_SENSOR1_PRESENT = EXT_SENSOR1_PRESENT ? EXT_SENSOR1_PRESENT : sensors.getAddress(EXT_SENSOR1, 1);
-  EXT_SENSOR2_PRESENT = EXT_SENSOR2_PRESENT ? EXT_SENSOR2_PRESENT : sensors.getAddress(EXT_SENSOR2, 0);
-  // .. for 3 or more sensors you may want to establish the order by trial and error instead
-
-  // No luck?
+// not needed
+ // EXT_SENSOR1_PRESENT = sensors.getAddress(EXT_SENSOR1, 0);
+ 
+ // bus info
   if (debug){
     
-    if (!EXT_SENSOR1_PRESENT) 
-      Serial.println("Unable to find address for DS18B20 External 1... check hard coded address");
-    else 
-      Serial.println("Found DS18B20 External 1");
-      
-    
-    if (!EXT_SENSOR2_PRESENT) 
-      Serial.println("Unable to find address for DS18B20 External 2... check hard coded address");
-    else 
-      Serial.println("Found DS18B20 External 2");
-      
-    // .. and for sensor 3, etc...
+     // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(numberOfDevices, DEC);
+  Serial.println(" devices.");
+
+  // report parasite power requirements
+  Serial.print("Parasite power is: "); 
+  if (sensors.isParasitePowerMode()) Serial.println("ON");
+  else Serial.println("OFF");
+  } 
+ 
+ // Loop through each device, print out address
+  for(int i=0;i<numberOfDevices; i++)
+  {
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i))
+	{
+		if (debug) {
+                  Serial.print("Found device ");
+		  Serial.print(i, DEC);
+		  Serial.print(" with address: ");
+		  printAddress(tempDeviceAddress); // just prints sensor adress on serial bus
+		  Serial.println();
+		
+		  Serial.print("Setting resolution to ");
+		  Serial.println(TEMPERATURE_PRECISION, DEC);
+                }
+		
+		// set the resolution to TEMPERATURE_PRECISION bit (Each Dallas/Maxim device is capable of several different resolutions)
+		sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
+		
+		if (debug) {
+		  Serial.print("Resolution actually set to: ");
+		  Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
+		  Serial.println();
+                }
+	}else{
+              
+	      if (debug) {
+		Serial.print("Found ghost device at ");
+		Serial.print(i, DEC);
+		Serial.print(" but could not detect address. Check power and cabling");
+              }
+	}
   }
+
+
+
+    
+    
+   // if (!EXT_SENSOR1_PRESENT){ }
 
   // Switch off for now
   digitalWrite(DS18B20_PWR, LOW);
 }
 
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
 
+//////////////////////////////////////////////////
 /** 
  * If we don't have at least one sensor available, it's sleep time
  */
 void validate_sensor_presence()
 {
-  if (!DHT_PRESENT && !EXT_SENSOR1_PRESENT && !EXT_SENSOR2_PRESENT) 
+  if (!DHT_PRESENT && !numberOfDevices >= 1) 
   {
     if (debug) {
       
@@ -338,7 +388,7 @@ void validate_sensor_presence()
   }
 }
 
-
+//////////////////////////////////////////////////
 /**
  * Convenience method; battery reading
  */
@@ -366,51 +416,65 @@ void take_dht22_reading()
   digitalWrite(DHT_PWR, LOW); 
 }
 
-
+//////////////////////////////////////////////////
 /**
  * Convenience method; read from all DS18B20s
- * You will need to extend this to read from any extra sensors.
+ * 
  */
 void take_ds18b20_reading () 
 {
   // Power up
   digitalWrite(DS18B20_PWR, HIGH); 
   dodelay(50); 
-
-  // Set precision to desired value
-  sensors.setResolution(EXT_SENSOR1, TEMPERATURE_PRECISION);
-  sensors.setResolution(EXT_SENSOR2, TEMPERATURE_PRECISION);
-
-  // Get readings. We must wait for ASYNC_DELAY due to power-saving (waitForConversion = false)
-  sensors.requestTemperatures();                                   
-  dodelay(ASYNC_DELAY); 
-  float temp1=(sensors.getTempC(EXT_SENSOR1));
-  float temp2=(sensors.getTempC(EXT_SENSOR2)); 
+  
+  
+// call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  
+  
+  // Loop through each device, print out temperature data
+  for(int i=0;i<numberOfDevices; i++)
+  {
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i))
+	{	
+           sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
+           
+           // Get readings. We must wait for ASYNC_DELAY due to power-saving (waitForConversion = false)
+           sensors.requestTemperatures();                                   
+           dodelay(ASYNC_DELAY); 
+           float temp1=(sensors.getTempC(tempDeviceAddress));
+           
+           // Payload will maintain previous reading unless the temperature is within range.
+           if (temperature_in_range(temp1))
+           rfPayload[3].onewireTemp[i] = temp1 * 10; 
+	} 
+	//else ghost device! Check your power requirements and cabling
+	
+  }
 
   // Power down
   digitalWrite(DS18B20_PWR, LOW);
-
-  // Payload will maintain previous reading unless the temperature is within range.
-  if (temperature_in_range(temp1))
-    rfPayload.externalTemp1= temp1 * 10;   
-
-  if (temperature_in_range(temp2))
-    rfPayload.externalTemp2= temp2 * 10;   
+ 
 }
 
+
+//////////////////////////////////////////////////
 /**
- * To save power, we go to sleep between readings
+ * validate that the provided temperature is within acceptable bounds.
  */
-void sleep_until_next_reading(){
-  byte oldADCSRA=ADCSRA;
-  byte oldADCSRB=ADCSRB;
-  byte oldADMUX=ADMUX;   
-  Sleepy::loseSomeTime(SECS_BETWEEN_READINGS*1000);  
-  ADCSRA=oldADCSRA; // restore ADC state
-  ADCSRB=oldADCSRB;
-  ADMUX=oldADMUX;
+boolean temperature_in_range(float temp)
+{
+  // Only accept the reading if it's within a desired range.
+  float minimumTemp = -40.0;
+  float maximumTemp = 125.0;
+
+  return temp > minimumTemp && temp < maximumTemp;
 }
 
+//////////////////////////////////////////////////
 /**
  * For debugging purposes: print the payload as it will shortly be sent to the emonBASE
  * Wise to extend this if you have extra sensors wired in.
@@ -426,25 +490,16 @@ void print_payload()
   Serial.print(rfPayload.battery/10.0);
   Serial.println("V");
   
-  Serial.print("  External sensor 1: ");
 
-  if (EXT_SENSOR1_PRESENT){
+ /* if (EXT_SENSOR1_PRESENT){
     Serial.print( rfPayload.externalTemp1/10.0); 
     Serial.println("C");
   }
   else {
     Serial.println(" not present");
   }
-    
-  Serial.print("  External sensor 2: ");
-
-  if (EXT_SENSOR2_PRESENT){
-    Serial.print( rfPayload.externalTemp2/10.0); 
-    Serial.println("C");
-  }
-  else {
-    Serial.println(" not present");
-  }
+  */
+  
   
   if (DHT_PRESENT){
     Serial.print("  Internal DHT22 temperature: ");
@@ -461,7 +516,7 @@ void print_payload()
   Serial.println();
 }
 
-
+//////////////////////////////////////////////////
 /**
  * Dumps useful intro to serial
  */
@@ -498,30 +553,27 @@ void print_welcome_message()
   dodelay(100);
 }
 
-
+//////////////////////////////////////////////////
 /**
- * validate that the provided temperature is within acceptable bounds.
+ * Turn off what we don't need.
+ * see http://www.nongnu.org/avr-libc/user-manual/group__avr__power.html
  */
-boolean temperature_in_range(float temp)
+void reduce_power()
 {
-  // Only accept the reading if it's within a desired range.
-  float minimumTemp = -40.0;
-  float maximumTemp = 125.0;
+  ACSR |= (1 << ACD);              // Disable Analog comparator    
+  power_twi_disable();             // Disable the Two Wire Interface module.
 
-  return temp > minimumTemp && temp < maximumTemp;
+  power_timer1_disable();          // Timer 1
+  power_spi_disable();             // Serial peripheral interface
+
+    if (!debug){
+    power_usart0_disable();        // Disable serial UART if not connected
+  }  
+
+  power_timer0_enable();           // Necessary for the DS18B20 library.
 }
 
-
-/**
- * Flash the LED for the stated period
- */
-void flash_led (int duration){
-  digitalWrite(LED,HIGH);
-  dodelay(duration);
-  digitalWrite(LED,LOW); 
-}
-
-
+//////////////////////////////////////////////////
 /**
  * Power-friendly delay
  */
@@ -537,4 +589,19 @@ void dodelay(unsigned int ms)
   ADCSRB=oldADCSRB;
   ADMUX=oldADMUX;
 }
+
+//////////////////////////////////////////////////
+/**
+ * To save power, we go to sleep between readings
+ */
+void sleep_until_next_reading(){
+  byte oldADCSRA=ADCSRA;
+  byte oldADCSRB=ADCSRB;
+  byte oldADMUX=ADMUX;   
+  Sleepy::loseSomeTime(SECS_BETWEEN_READINGS*1000);  
+  ADCSRA=oldADCSRA; // restore ADC state
+  ADCSRB=oldADCSRB;
+  ADMUX=oldADMUX;
+}
+
 
