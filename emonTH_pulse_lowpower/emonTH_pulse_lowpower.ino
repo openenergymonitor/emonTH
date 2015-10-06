3b8ad4a7fe69b1dbdd813ba84bac2016bbebbf6e
TrystanLea <glyn.hudson@openenergymonitor.org>
2015-10-06 23:40:36 +0100

    EmonTH low power pulse counting example by Eric AMANN replacing the not so low power example that was before

--- emonTH_pulse_lowpower/emonTH_pulse_lowpower.ino
+++ emonTH_pulse_lowpower/emonTH_pulse_lowpower.ino
@@ -1,83 +1,167 @@
-// Warm1,2 & 3 pulse count for Kamstrup and gas.  
-bool debug = 1;
+// Low power emonTH pulse counting example
+// Author: Eric_AMANN
+// see forum post: http://openenergymonitor.org/emon/node/10834
 
-#define RF69_COMPAT 1
-#include <JeeLib.h>
+/*Recommended node ID allocation
+------------------------------------------------------------------------------------------------------------
+-ID-	-Node Type- 
+0	- Special allocation in JeeLib RFM12 driver - reserved for OOK use
+1-4     - Control nodes 
+5-10	- Energy monitoring nodes
+11-14	--Un-assigned --
+15-16	- Base Station & logging nodes
+17-30	- Environmental sensing nodes (temperature humidity etc.)
+31	- Special allocation in JeeLib RFM12 driver - Node31 can communicate with nodes on any network group
+-------------------------------------------------------------------------------------------------------------
+*/
 
+#define RF_freq RF12_433MHZ                                                // Frequency of RF12B module can be RF12_433MHZ, RF12_868MHZ or RF12_915MHZ.
+const int nodeID = 21;                                                     // emonTh RFM12B node ID
+const int networkGroup = 210;                                              // emonTh RFM12B wireless network group
+const int UNO = 1;                                                         // Set to 0 if your not using the UNO bootloader (i.e using Duemilanove)
+                                                                           // All Atmega's shipped from OpenEnergyMonitor come with Arduino Uno bootloader
+#include <avr/wdt.h>                                                       // the UNO bootloader 
+
+#include <JeeLib.h>                                                        // Download JeeLib: http://github.com/jcw/jeelib
+ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 #include <avr/power.h>
 #include <avr/sleep.h>
-ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
-const int LED=            9;
-const int BATT_ADC=       1;
+typedef struct { unsigned int power, pulse, totalpulse, battery; } PayloadTH;
+PayloadTH emonth;                                                          // neat way of packaging data for RF comms
 
-typedef struct {                                                      // RFM12B RF payload datastructure
-  	  unsigned long pulsecount;
-          int battery;       	                                      
-} Payload;
-Payload emonth;
+const  unsigned long WDT_PERIOD = 80;                                      // mseconds.
+const  unsigned long WDT_MAX_NUMBER = 600;                                 // Data sent after WDT_MAX_NUMBER periods of  WDT_PERIOD ms without pulses
+                                                                           //                              OR
+const  unsigned long PULSE_MAX_NUMBER = 100;                               // Data sent after PULSE_MAX_NUMBER pulses
+const  unsigned long PULSE_MAX_DURATION = 50;                              // Sensor is powered off during PULSE_MAX_DURATION mseconds after a pulse.
+ 
+const int LEDpin = 9;                                                      // emonTH LED pin
+const int BATT_ADC = 1;
+const int PULSE_SENSOR_PWR = 5;                                            // **Labelled DS18B20 power on emonTH board**
 
-unsigned long pulsetime = 0;
 
-// minimum width of interrupt pulse (default pulse output meters = 100ms)
-const byte min_pulsewidth= 110;
+volatile unsigned long pulseCount ;
+volatile unsigned long elapsed_time;
+int BATT_ADC_number;
+unsigned long WDT_number;
 
-void setup() {
-  pinMode(LED,OUTPUT); digitalWrite(LED,HIGH);
-  
-  if (debug) {
-    Serial.begin(9600);
-    Serial.println("emonTH Pulse Counting example");
-    delay(100);
-  } else {
-    power_usart0_disable();
-  }
-  power_twi_disable();
-  
-  rf12_initialize(3,RF12_433MHZ,210);
+boolean  debug;
+boolean  p;                                                                 // flag for new pulse
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("EmonTH Pulse conting Low Power V1.0c");
+  Serial.print("Goup Id: ");
+  Serial.println(networkGroup);
+  Serial.print("Node Id: ");
+  Serial.println(nodeID);
+  Serial.print("Sending data after ");
+  Serial.print(PULSE_MAX_NUMBER);
+  Serial.println(" pulses");
+  Serial.print("  or  ");
+  Serial.print(WDT_MAX_NUMBER);
+  Serial.print(" periods of ");
+  Serial.print(WDT_PERIOD);
+  Serial.println(" ms whitout any pulses");
+  delay(100);
+
+  Serial.println("Initializing RF..");
+  delay(100);             
+  rf12_initialize(nodeID, RF_freq, networkGroup);                         // initialize RF
   rf12_sleep(RF12_SLEEP);
+  Serial.println("RF ready");
+
+  pinMode(LEDpin, OUTPUT);                                                // Setup indicator LED
+  pinMode(PULSE_SENSOR_PWR,OUTPUT);                                       // Optical pulse sensor power
+  digitalWrite(PULSE_SENSOR_PWR, HIGH);
+    
+  emonth.power=0;
+  emonth.pulse=0;
+  emonth.totalpulse=0;
+  emonth.battery=0;
+  pulseCount=0;
+    
+  WDT_number=0;
   
-  emonth.pulsecount = 0;
-  
-  attachInterrupt(1, onPulse, FALLING);
-  digitalWrite(LED,LOW);
+  if (Serial) debug = 1; else debug=0;                                    // if serial UART to USB is connected show debug O/P. If not then disable serial
+  p=0;
+  attachInterrupt(1, onPulse, RISING);                                    // KWH interrupt attached to IRQ 0  = Digita 2 - hardwired to terminal block 
+
+ 
+ }
+
+void loop() 
+{
+  // if a pulse just came, wait PULSE_MAX_DURATION ms to power on the optical pulse sensor 
+  if (p){
+    Sleepy::loseSomeTime(PULSE_MAX_DURATION);
+    digitalWrite(PULSE_SENSOR_PWR, HIGH);
+    p=0;
+  }
+
+  if (Sleepy::loseSomeTime(WDT_PERIOD)==1) {                              // true if no pulse occured during the WDT
+    WDT_number++;                                                         // number of WDT normally ended since the last RF sent
+    if (debug) {
+      Serial.print(".");
+      delay(1);
+    }
+  }  
+  else if (debug)     
+  { 
+    Serial.print("INT ");
+    delay(5);
+    Serial.println(pulseCount);
+    delay(5);
+  }
+
+
+  if (WDT_number>=WDT_MAX_NUMBER || pulseCount>=PULSE_MAX_NUMBER) {
+    cli(); // Disable interrupt just in case pulse comes in while we are updating the count
+    emonth.totalpulse += (unsigned int) pulseCount;
+    emonth.pulse = (unsigned int) pulseCount;      
+    pulseCount = 0; 
+    sei();
+
+    emonth.battery=(unsigned int)(analogRead(BATT_ADC)*0.03225806);       //read battery voltage, convert ADC to volts x10
+
+    if (debug==1) {
+      Serial.print("Sending ");delay(5);
+      Serial.print(emonth.pulse);delay(5);
+      Serial.print("/");delay(5);
+      Serial.print(emonth.totalpulse);delay(5);
+      Serial.print("/");delay(5);
+      Serial.print(emonth.power);delay(5);
+      Serial.print("W");delay(5);
+      Serial.print("/");delay(5);
+      Serial.print(emonth.battery);delay(5);
+      Serial.println("");delay(5);
+    }   
+
+    send_rf_data();                          // *SEND RF DATA* - see emontx_lib
+    WDT_number=0;
+  }
 }
 
-void loop()
-{
-  // STATUS COUNT
-  emonth.battery = analogRead(BATT_ADC) * 3.32;                    //read battery voltage, convert ADC to volts x10
 
-  if (debug) {
-    Serial.print("Count: ");
-    Serial.println(emonth.pulsecount);
-    delay(25);
-    Serial.print("Battery: ");
-    Serial.println(emonth.battery);
-    delay(25);
-  }
-  
-  power_spi_enable();  
+// The interrupt routine - runs each time a rising edge of a pulse is detected
+void onPulse()                  
+{
+  digitalWrite(PULSE_SENSOR_PWR, LOW);       // optical pulse sensor power off
+  p=1;                                       // flag for new pulse set to true
+  pulseCount++;                              // number of pulses since the last RF sent
+}
+
+
+
+void send_rf_data()
+{
+  power_spi_enable(); 
   rf12_sleep(RF12_WAKEUP);
-  delay(20);
-  rf12_sendNow(0, &emonth, sizeof emonth);
+  rf12_sendNow(0, &emonth, sizeof emonth);   // send temperature data via RFM12B using new rf12_sendNow wrapper
   rf12_sendWait(2);
   rf12_sleep(RF12_SLEEP);
-  emonth.pulsecount++;   // TRY TO GET NUMBER SENT
-  power_spi_disable();  
-  
-  Sleepy::loseSomeTime(60000);  //TAKEN ONE ZERO OFF FOR TESTING!!!
-  
+  power_spi_disable(); 
 }
 
-void onPulse() {
-  
-  if ( (millis() - pulsetime) > min_pulsewidth) {
-    emonth.pulsecount++;
-    pulsetime=millis(); 
-  }
-  
-  digitalWrite(LED, HIGH); 
-  delay(2); 
-  digitalWrite(LED, LOW);
-}
